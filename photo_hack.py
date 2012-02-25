59c6714e59fc8d95ae2990de5724d5ffcdc51073
David Xia <david@davidxia.com>
2012-02-25 18:25:15 -0500

    can now save uploaded photos and return json response

--- photo_hack.py
+++ photo_hack.py
@@ -1,30 +1,34 @@
-import sys, json
+import sys, json, time
 from pprint import pprint
-from bottle import route, run, request, response
+from bottle import route, run, request, response, static_file, error
 sys.path.append('/home/david/photo-hack')
 import backend_stuff
 
 @route('/', method='POST')
 def index():
-    data = request.files.data
-    if data and data.file:
-        raw = data.file.read() # This is dangerous for big files
-        filename = data.filename
-        
-        # decode data using base64 codec, write to a file
-        fh = open('imageToSave.jpg', 'wb')
-        #fh.write(raw.decode('base64'))
-        fh.write(raw)
+    image = request.POST.get('image')
+    lat = request.POST.get('lat')
+    lng = request.POST.get('lng')
+    filename = 'u' + str(int(time.time())) + '.jpeg'
+
+    if image:
+        fh = open('uploaded_images/' + filename, 'wb')
+        fh.write(image.decode('base64'))
         fh.close()
 
-        return "Hello You uploaded %s (%d bytes)." % (filename, len(raw))
-
     response.content_type = 'application/json'
-    venues = backend_stuff.main()
-    venues_json = json.dumps(venues)
-    return venues_json
+    return {'image_url': 'http://dev.ragemyface.com/uploaded_images/' + filename, 'venue_id': '123'}
+
 
 @route('/hello')
 def hello():
-    return "Hello World!"
+    response.content_type = 'application/json'
+    return {'image_url': 'http://dev.ragemyface.com/uploaded_images/', 'venue_id': 123}
 
+@route('/uploaded_images/<filename>')
+def send_image(filename):
+    return static_file(filename, root='/home/david/photo-hack/uploaded_images')
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
