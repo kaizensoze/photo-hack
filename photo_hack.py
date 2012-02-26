c9b1bb5e3363af393777241be9dca40107d5982c
duckspeaker <gallo.j@gmail.com>
2012-02-25 23:00:00 -0500

    refactored backend code

--- photo_hack.py
+++ photo_hack.py
@@ -1,32 +1,57 @@
-import sys, json, time
+
+import json
+import sys
+import threading
+import time
+
 from pprint import pprint
+
 from bottle import route, run, request, response, static_file, error
+import backend
+
 sys.path.append('/home/david/photo-hack')
-import backend_stuff
 
 
 UPLOADED_IMAGES_PATH = '/home/david/photo-hack/uploaded_images'
 COMPARE_IMAGES_PATH = '/home/david/photo-hack/compare_images'
 BASE_URL = 'http://dev.ragemyface.com/'
 
-@route('/', method='POST')
-def index():
+
+@route('/upload', method='POST')
+def upload():
+    # upload params
     image = request.POST.get('image')
-    filename = request.POST.get('filename')
+    #filename = request.POST.get('filename')
     lat = request.POST.get('lat')
     lng = request.POST.get('lng')
+
+    gps_loc = (lat, lng)
+
+    # generate filename to save image to disk
     if not filename:
         filename = 'u' + str(int(time.time())) + '.jpeg'
 
+    # save image to disk
     if image:
         fh = open('uploaded_images/' + filename, 'wb')
         fh.write(image.decode('base64'))
         fh.close()
 
-    # call backend_stuff.findMatch(filename, (lat, lng))
+    match_result = backend.getMatch(filename, gps_loc)
+    match_result["filepath"] = filename
 
     response.content_type = 'application/json'
-    return {'image_url': BASE_URL + filename, 'venue_id': '123'}
+    return json.dumps(match_result)
+
+
+@route('/send_postcard')
+def send_postcard():
+    image_url = request.GET.get('image_url')
+    venue_name = request.GET.get('venue_name')
+
+    threading.Timer(0, backend.sendPostCard, [image_url, venue_name]).start()
+
+    return image_url
 
 
 @route('/uploaded_images/<filename>')
@@ -37,12 +62,6 @@ def send_image(filename):
 @route('/compare_images/<filename>')
 def compare_images(filename):
     return static_file(filename, root=COMPARE_IMAGES_PATH)
-
-
-@route('/send_postcard')
-def send_postcard():
-    image_url = request.GET.get('image_url')
-    return image_url
 
 
 @error(404)
